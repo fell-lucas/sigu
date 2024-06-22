795cc5f1f55e19f6fb30b77932712631c71d4f64
Lucas Fell <47724710+fell-lucas@users.noreply.github.com>
2024-06-21 21:43:36 -0300

    add cloudflare adapter, remove dotenv (#2)
    * add cloudflare adapter, remove dotenv
    
    * fix cloudflare
    
    * fix node
    
    * format

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,16 +1,14 @@
 import { Lucia } from 'lucia';
 import { dev } from '$app/environment';
-import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
 import { sessionTable, userTable } from './db/schema';
-
-config({ path: '.env' });
+import { env } from '$env/dynamic/private';
 
 const client = createClient({
-	url: process.env.TURSO_CONNECTION_URL!,
-	authToken: process.env.TURSO_AUTH_TOKEN!
+	url: env.TURSO_CONNECTION_URL,
+	authToken: env.TURSO_AUTH_TOKEN
 });
 
 export const db = drizzle(client);
