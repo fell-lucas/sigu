d9460781fd8e0dfc542dd1b1b5640f1f525c7f4e
LucasFell <lucas.fell@partner.bmw.com.br>
2024-06-25 16:32:31 -0300

    add professorId to get course

--- src/lib/server/trpc/routers/course/getCourse.ts
+++ src/lib/server/trpc/routers/course/getCourse.ts
@@ -1,8 +1,8 @@
 import { db } from '$lib/server/auth';
 import { courseTable, userTable } from '$lib/server/db/schema';
-import { publicProcedure } from '../../t';
-import { messageDatabaseQueryError } from '$lib/server/exceptions';
 import { eq } from 'drizzle-orm';
+import { setFlash } from 'sveltekit-flash-message/server';
+import { publicProcedure } from '../../t';
 
 export const getCourse = publicProcedure.query(async ({ ctx }) => {
 	try {
@@ -11,6 +11,7 @@ export const getCourse = publicProcedure.query(async ({ ctx }) => {
 				id: courseTable.id,
 				name: courseTable.name,
 				description: courseTable.description,
+				professorId: userTable.id,
 				professorName: userTable.name,
 				slotsCount: courseTable.slotsCount,
 				startDate: courseTable.startDate,
@@ -24,6 +25,11 @@ export const getCourse = publicProcedure.query(async ({ ctx }) => {
 		return course[0];
 	} catch (error) {
 		console.error(error);
-		return messageDatabaseQueryError(ctx);
+		setFlash(
+			{ type: 'error', message: 'Algo deu errado ao acessar o banco de dados.' },
+			ctx.event.cookies
+		);
+
+		return undefined;
 	}
 });
