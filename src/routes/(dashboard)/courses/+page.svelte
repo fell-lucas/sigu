ab1a82f3ed4eaf2f267ffbcc7ebe20a666068c3a
IgorDalepiane <igordalepiane@gmail.com>
2024-06-25 10:21:51 -0300

    style: page heading

--- src/routes/(dashboard)/courses/+page.svelte
+++ src/routes/(dashboard)/courses/+page.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import { AppBar } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+	import PhPlus from '~icons/ph/plus';
+
 </script>
 
 <AppBar>
 	<svelte:fragment slot="headline">
-		<h3 class="h3 w-full">Meus cursos</h3>
+		<div class="flex w-full justify-between">
+			<h3 class="h3">Cursos</h3>
+      {#if $page.url.pathname === '/courses'}
+			<a type="button" class="variant-filled btn" href="/courses/new" >
+				<span><PhPlus class="w-full" /></span>
+				<span>Novo Curso</span>
+			</a>
+      {/if}
+		</div>
 	</svelte:fragment>
 </AppBar>
