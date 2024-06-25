29319f478a5abda5d73991bcd1104e7e9fa09a25
IgorDalepiane <igordalepiane@gmail.com>
2024-06-25 18:21:09 -0300

    style: format , lint

--- src/routes/(dashboard)/courses/+page.svelte
+++ src/routes/(dashboard)/courses/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { AppBar, ProgressRadial } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+	import { AppBar } from '@skeletonlabs/skeleton';
 	import { superForm } from 'sveltekit-superforms';
 	import PhPlus from '~icons/ph/plus';
 	import PhUserBold from '~icons/ph/user-bold';
 	import { EnrollmentStatus } from '../../../lib/db-enums';
-	import { page } from '$app/stores';
 
 	export let data;
 	let deleteConfirmation: string | null = null;
@@ -36,66 +36,66 @@
 	<h5 class="h5 font-semibold">Cursos disponíveis</h5>
 	<hr class="!border-t-2" />
 	<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-				{#each data.courses as course}
-					<div class="text-toke card flex flex-col justify-between overflow-hidden shadow-xl">
-						<a class="space-y-4 p-4" href="/courses/{course.id}">
-							<h3 class="h3 line-clamp-1 overflow-ellipsis">{course.name}</h3>
-							<article>
-								<p class="line-clamp-2 opacity-75">
-									{course.description}
-								</p>
-								<p class="opacity-75">
-									<strong>Data de início:</strong>
-									{new Date(course.startDate).toLocaleDateString()}
-								</p>
-								<p class="opacity-75">
-									<strong>Data de fim:</strong>
-									{new Date(course.endDate).toLocaleDateString()}
-								</p>
-								<p class="opacity-75">
-									<strong>Número de vagas:</strong>
-									{course.slotsCount}
-								</p>
-							</article>
-						</a>
-						<footer class="items-center">
-							<hr class="opacity-50" />
-							<div class="flex flex-row justify-between space-x-4 p-4">
-								<span class="flex flex-row items-center gap-2 font-semibold">
-									<span><PhUserBold class="w-full" /></span>
-									<span>{course.professorName}</span>
-								</span>
-								<span class="items-center gap-2 font-semibold">
-									{#if $page.data.session?.userId !== course.professorId}
-										{#if course.enrollmentStatus == EnrollmentStatus.ENROLLED}
-											<span class="text-green-500 mr-4">Inscrito</span>
-										{:else if course.slotsCount === 0}
-											<span class="text-red-500 btn variant-ghost">Sem vagas</span>
+		{#each data.courses as course}
+			<div class="text-toke card flex flex-col justify-between overflow-hidden shadow-xl">
+				<a class="space-y-4 p-4" href="/courses/{course.id}">
+					<h3 class="h3 line-clamp-1 overflow-ellipsis">{course.name}</h3>
+					<article>
+						<p class="line-clamp-2 opacity-75">
+							{course.description}
+						</p>
+						<p class="opacity-75">
+							<strong>Data de início:</strong>
+							{new Date(course.startDate).toLocaleDateString()}
+						</p>
+						<p class="opacity-75">
+							<strong>Data de fim:</strong>
+							{new Date(course.endDate).toLocaleDateString()}
+						</p>
+						<p class="opacity-75">
+							<strong>Número de vagas:</strong>
+							{course.slotsCount}
+						</p>
+					</article>
+				</a>
+				<footer class="items-center">
+					<hr class="opacity-50" />
+					<div class="flex flex-row justify-between space-x-4 p-4">
+						<span class="flex flex-row items-center gap-2 font-semibold">
+							<span><PhUserBold class="w-full" /></span>
+							<span>{course.professorName}</span>
+						</span>
+						<span class="items-center gap-2 font-semibold">
+							{#if $page.data.session?.userId !== course.professorId}
+								{#if course.enrollmentStatus == EnrollmentStatus.ENROLLED}
+									<span class="mr-4 text-green-500">Inscrito</span>
+								{:else if course.slotsCount === 0}
+									<span class="variant-ghost btn text-red-500">Sem vagas</span>
+								{:else}
+									<form use:enhance method="POST">
+										<input type="hidden" name="courseId" value={course.id} />
+										{#if deleteConfirmation == course.id}
+											<button type="submit" class="variant-filled btn btn-sm">
+												<span>Confirma?</span>
+											</button>
 										{:else}
-											<form use:enhance method="POST">
-												<input type="hidden" name="courseId" value={course.id} />
-												{#if deleteConfirmation == course.id}
-													<button type="submit" class="variant-filled btn-sm btn">
-														<span>Confirma?</span>
-													</button>
-												{:else}
-													<button
-														type="button"
-														class="variant-filled btn-sm btn"
-														onclick={() => (deleteConfirmation = course.id)}
-													>
-														<span>Inscrever-se</span>
-													</button>
-												{/if}
-											</form>
+											<button
+												type="button"
+												class="variant-filled btn btn-sm"
+												onclick={() => (deleteConfirmation = course.id)}
+											>
+												<span>Inscrever-se</span>
+											</button>
 										{/if}
-									{/if}
-								</span>
-							</div>
-						</footer>
+									</form>
+								{/if}
+							{/if}
+						</span>
 					</div>
-				{:else}
-					<p>Nenhum curso disponível.</p>
-				{/each}
+				</footer>
+			</div>
+		{:else}
+			<p>Nenhum curso disponível.</p>
+		{/each}
 	</div>
 </main>
