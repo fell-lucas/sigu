6415bdaf2c17cb3ac918761482eabb74859880ae
IgorDalepiane <igordalepiane@gmail.com>
2024-06-25 11:21:56 -0300

    feat: loading action for courses

--- src/routes/(dashboard)/courses/+layout.server.js
+++ src/routes/(dashboard)/courses/+layout.server.js
@@ -3,7 +3,6 @@ import { createCaller } from '$lib/server/trpc/router';
 
 export const load = async (event) => {
 	const caller = createCaller(await createContext(event));
-	const courses = await caller.course.getAll();
 
-	return { courses };
+	return { courses: caller.course.getAll() }; 
 };
--- src/routes/(dashboard)/courses/+page.svelte
+++ src/routes/(dashboard)/courses/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { AppBar } from '@skeletonlabs/skeleton';
+	import { AppBar, ProgressRadial } from '@skeletonlabs/skeleton';
 	import { page } from '$app/stores';
 	import PhPlus from '~icons/ph/plus';
 
@@ -10,37 +10,69 @@
 	<svelte:fragment slot="headline">
 		<div class="flex w-full justify-between">
 			<h3 class="h3">Cursos</h3>
-      {#if $page.url.pathname === '/courses'}
-			<a type="button" class="variant-filled btn" href="/courses/new" >
-				<span><PhPlus class="w-full" /></span>
-				<span>Novo Curso</span>
-			</a>
-      {/if}
+			{#if $page.url.pathname === '/courses'}
+				<a type="button" class="variant-filled btn" href="/courses/new">
+					<span><PhPlus class="w-full" /></span>
+					<span>Novo Curso</span>
+				</a>
+			{/if}
 		</div>
 	</svelte:fragment>
 </AppBar>
 
-<main class="flex	 flex-row max-w-5xl mt-8 gap-4">
-  {#if data.courses.length === 0}
-    <p class="text-center">Nenhum curso disponível.</p>
-  {:else}
-    {#each data.courses as course}
-      <div class="card p-4 border rounded shadow">
-        <h4 class="text-lg font-semibold">{course.name}</h4>
-        <p><strong>Professor:</strong> {course.professorName}</p>
-        <p><strong>Numero de vagas:</strong> {course.slotsCount}</p>
-        <p><strong>Data de início:</strong> {new Date(course.startDate).toLocaleDateString()}</p>
-        <p><strong>Data de fim:</strong> {new Date(course.endDate).toLocaleDateString()}</p>
-      </div>
-    {/each}
-  {/if}
+<main class="mx-4 mt-8 flex flex-col gap-4">
+	<h5 class="h5 font-semibold">Cursos disponíveis</h5>
+	<hr class="!border-t-2" />
+	<div class="flex flex-row gap-4">
+		{#await data.courses}
+			<ProgressRadial class="w-8" />
+		{:then courses}
+			{#if courses.length === 0}
+				<p class="text-center">Nenhum curso disponível.</p>
+			{:else}
+				{#each courses as course}
+					<div class="card text-token max-w-[400px] overflow-hidden shadow-xl">
+						<div class="space-y-4 p-4">
+							<h3 class="h3">{course.name}</h3>
+							<article>
+								<p class="opacity-75">
+									This showcases Skeleton's Card, Typography, Chips, and Divider elements.
+								</p>
+							</article>
+						</div>
+						<hr class="opacity-50" />
+						<footer class="flex items-center justify-between space-x-4 p-4">
+							<span class="variant-soft chip hover:variant-filled">
+								<i class="fa-solid fa-heart"></i>
+								<span>Like</span>
+							</span>
+							<span class="variant-soft chip hover:variant-filled">
+								<i class="fa-solid fa-paperclip"></i>
+								<span>Save</span>
+							</span>
+						</footer>
+					</div>
+					<div class="card rounded border p-4 shadow">
+						<h4 class="text-lg font-semibold">{course.name}</h4>
+						<p><strong>Professor:</strong> {course.professorName}</p>
+						<p><strong>Numero de vagas:</strong> {course.slotsCount}</p>
+						<p>
+							<strong>Data de início:</strong>
+							{new Date(course.startDate).toLocaleDateString()}
+						</p>
+						<p><strong>Data de fim:</strong> {new Date(course.endDate).toLocaleDateString()}</p>
+					</div>
+				{/each}
+			{/if}
+		{/await}
+	</div>
 </main>
 
 <style>
-  .card {
-    transition: transform 0.2s;
-  }
-  .card:hover {
-    transform: scale(1.02);
-  }
+	.card {
+		transition: transform 0.2s;
+	}
+	.card:hover {
+		transform: scale(1.02);
+	}
 </style>
