2c7bce5747937fd4fc973c38bcf2e205c421c73c
IgorDalepiane <igordalepiane@gmail.com>
2024-06-25 18:09:04 -0300

    refactor: using enuns on middleware

--- src/lib/server/trpc/middlewares/isAdmin.ts
+++ src/lib/server/trpc/middlewares/isAdmin.ts
@@ -1,9 +1,10 @@
 import { setFlash } from 'sveltekit-flash-message/server';
 import type { MiddlewareFn } from '../t';
 import { redirect } from '@sveltejs/kit';
+import { UserRole } from '../../../db-enums';
 
 export const isAdmin: MiddlewareFn = async ({ ctx, next }) => {
-	if (ctx.event.locals.session!.role !== 'admin') {
+	if (ctx.event.locals.session!.role !== UserRole.ADMIN) {
 		setFlash({ message: 'Você não possui permissão para isso', type: 'error' }, ctx.event.cookies);
 		throw redirect(303, '/');
 	}
