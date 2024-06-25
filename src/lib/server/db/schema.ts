9902bf47ab65f96946c904bdf0e7912f434fca10
IgorDalepiane <igordalepiane@gmail.com>
2024-06-25 11:22:21 -0300

    chore: schema absolute path, drizzle studio

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { UserRole } from '$lib/db-enums';
+import { UserRole } from '../../db-enums';
 import { sql } from 'drizzle-orm';
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
