327098cfa5e4e26ab0c4d00b81883bcb9c8eab8f
Igor Costa <igor.costa@partner.bmw.com.br>
2024-07-16 17:42:01 -0300

    test: new tests for course creation

--- src/e2e/create-course.spec.ts
+++ src/e2e/create-course.spec.ts
@@ -1,101 +1,118 @@
 import { UserRole } from '$lib/db-enums';
 import { db } from './utils/database';
-import { userTable } from '$lib/server/db/schema';
+import { userTable, courseTable, sessionTable } from '$lib/server/db/schema';
 import { hashPassword } from '$lib/server/utils';
 import { type Page, test as base, expect } from '@playwright/test';
 import { eq } from 'drizzle-orm';
 import { ulid } from 'ulidx';
 
 const test = base.extend<{
-	setup: { page: Page };
+	setup: { page: Page; professorId: string };
 }>({
 	setup: async ({ page }, use) => {
-		const userId = ulid();
-		const userEmail = 'newAdmin@gmail.com';
-		const userPassword = '#1Abcdef';
+		const adminId = ulid();
+		const adminEmail = Math.random().toString(36).substr(2, 8) + '@gmail.com';
+		const adminPassword = '#1Abcdef';
 		await db.insert(userTable).values({
-			email: userEmail,
-			id: userId,
-			name: 'AdminForTest',
-			password: await hashPassword(userPassword),
+			email: adminEmail,
+			id: adminId,
+			name: 'Admin For Test',
+			password: await hashPassword(adminPassword),
 			role: UserRole.ADMIN
 		});
 
+		const professorId = ulid();
+		const professorEmail = Math.random().toString(36).substr(2, 8) + '@gmail.com';
+		const professorPassword = '#1Abcdef';
+		await db.insert(userTable).values({
+			email: professorEmail,
+			id: professorId,
+			name: 'Professor For Test',
+			password: await hashPassword(professorPassword),
+			role: UserRole.PROFESSOR
+		});
+
 		await page.goto('/signin', { waitUntil: 'networkidle' });
-		await page.getByTestId('email-field').fill(userEmail);
-		await page.getByTestId('password-field').fill(userPassword);
+		await page.getByTestId('email-field').fill(adminEmail);
+		await page.getByTestId('password-field').fill(adminPassword);
 		await page.getByTestId('submit-btn').first().click();
 		await page.waitForTimeout(2000);
-		// await page.request.post('/signin', {
-		// 	headers: {
-		// 		host: 'localhost:4173',
-		// 		origin: 'http://localhost:4173',
-		// 		referrer: 'http://localhost:4173/signin'
-		// 	},
-		// 	form: {
-		// 		email: userEmail,
-		// 		password: userPassword
-		// 	}
-		// });
 
-		await use({ page });
+		await use({ page, professorId });
 
-		// await page.request.post('/?/signout', {
-		// 	headers: {
-		// 		host: 'localhost:4173',
-		// 		origin: 'http://localhost:4173',
-		// 		referrer: 'http://localhost:4173/signout'
-		// 	},
-		// 	form: {
-		// 		signout: 1,
-		// 		'cf-turnstile-response': ''
-		// 	}
-		// });
-
-		await db.delete(userTable).where(eq(userTable.id, userId));
+		await db.delete(sessionTable).where(eq(sessionTable.userId, adminId));
+		await db.delete(userTable).where(eq(userTable.id, adminId));
+		await db.delete(courseTable).where(eq(courseTable.professorId, professorId));
+		await db.delete(userTable).where(eq(userTable.id, professorId));
 	}
 });
 
-// test('should go to proponent`s profile page when clicking on the avatar image', async ({
-// 	setup: { page }
-// }) => {
-// 	await page.goto('/activities', { waitUntil: 'networkidle' });
-
-// 	await page.getByTestId('card-heading-user-image').first().click();
-// 	await page.waitForLoadState();
-
-// 	await expect(page.getByTestId('user-details-data')).toBeVisible();
-// 	await expect(page.getByTestId('user-details-statistics')).toBeVisible();
-// 	await expect(page.getByTestId('user-details-published-activities')).toBeVisible();
-// 	await expect(page.getByTestId('user-details-finished-activities')).toBeVisible();
-// });
-
-// New Course Registration
-// Positive Functional Tests
-// Registration with all valid fields filled: Verify that the course is created successfully.
-// Registration with long title and description: Ensure that the course is created and fields are stored correctly.
-// Registration with optional fields left blank: Verify that the course is created even with optional fields left blank.
-// Negative Functional Tests
-// Registration with the title field empty: Verify that an error message is displayed.
-// Registration with a duplicate title: Verify that an error message is displayed.
-// Registration with incorrect data format in a specific field: Verify that validation shows an error message.
-// Positive Structural Tests
-// Database performance check: Ensure that the course is inserted into the database quickly.
-// Data structure verification: Ensure that data is stored in the correct format.
-// Integration tests with other services (e.g., notification): Verify that registering a course correctly triggers associated services.
-// Negative Structural Tests
-// Simulated network failure during registration: Verify that an appropriate error message is displayed and data is not partially saved.
-// Insertion of corrupted data directly into the database: Verify that the system handles invalid data correctly.
-// Verification of race conditions during simultaneous registrations: Ensure that multiple simultaneous registrations do not cause inconsistencies.
-
-test('should create a new course', async ({ setup: { page } }) => {
+test('should create a new course', async ({ setup: { page, professorId } }) => {
 	await page.goto('/courses', { waitUntil: 'networkidle' });
 
 	await page.getByTestId('new-course-btn').first().click();
 	await page.waitForLoadState();
 
-	// await page.click('text=Salvar');
-	// await page.waitForLoadState();
+	await page.getByTestId('name-field').fill('Test course');
+	await page.getByTestId('description-field').fill('Test course description');
+	await page.getByTestId('slotsCount-field').fill('10');
+	await page.getByTestId('startDate-field').fill('2021-12-12');
+	await page.getByTestId('endDate-field').fill('2022-12-12');
+	await page.getByTestId('professor-field').selectOption(professorId);
+	await page.getByTestId('submit-btn').first().click();
 
-	await expect(1).toBe(0);
+	await expect(page.getByText('Curso criado com sucesso!')).toBeVisible();
+});
+
+test('should create a new course even if optional fields were not filled', async ({
+	setup: { page, professorId }
+}) => {
+	await page.goto('/courses', { waitUntil: 'networkidle' });
+
+	await page.getByTestId('new-course-btn').first().click();
+	await page.waitForLoadState();
+
+	// Description is a optional field
+	await page.getByTestId('name-field').fill('Test course');
+	await page.getByTestId('slotsCount-field').fill('10');
+	await page.getByTestId('startDate-field').fill('2021-12-12');
+	await page.getByTestId('endDate-field').fill('2022-12-12');
+	await page.getByTestId('professor-field').selectOption(professorId);
+	await page.getByTestId('submit-btn').first().click();
+
+	await expect(page.getByText('Curso criado com sucesso!')).toBeVisible();
+});
+
+test('should not create a new course if the startDate is after endDate', async ({
+	setup: { page, professorId }
+}) => {
+	await page.goto('/courses', { waitUntil: 'networkidle' });
+
+	await page.getByTestId('new-course-btn').first().click();
+	await page.waitForLoadState();
+
+	await page.getByTestId('name-field').fill('Test course');
+	await page.getByTestId('description-field').fill('Test course description');
+	await page.getByTestId('slotsCount-field').fill('10');
+	await page.getByTestId('startDate-field').fill('2022-02-02');
+	await page.getByTestId('endDate-field').fill('2022-01-01');
+	await page.getByTestId('professor-field').selectOption(professorId);
+	await page.getByTestId('submit-btn').first().click();
+
+	await expect(
+		page.getByText('A data de início deve ser anterior à data de término.')
+	).toBeVisible();
+});
+
+test('should not create a new course if required fields were not filled', async ({
+	setup: { page }
+}) => {
+	await page.goto('/courses', { waitUntil: 'networkidle' });
+
+	await page.getByTestId('new-course-btn').first().click();
+	await page.waitForLoadState();
+
+	await page.getByTestId('submit-btn').first().click();
+
+	await expect(page.getByText('Campo obrigatório')).toBeVisible();
 });
